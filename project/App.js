032ec6fe7f1b7fa93534425dde04a1af5d00c367
aynader <amr.nader@student.guc.edu.eg>
2022-11-02 22:07:44 +0100

    Schemas
    Creation of the project schema and database schema

---
+++ project/App.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const MongoURI = 'mongodb+srv://Error404:Error404TeamNotFound@jalp.jnqmtan.mongodb.net/?retryWrites=true&w=majority' ;
+const port = process.env.PORT || "8000";
+mongoose.connect(MongoURI)
+.then(()=>{
+  console.log("MongoDB is now connected!")
+})
+.catch(err => console.log(err));
+app.listen(port, () => {
+       console.log(`Listening to requests on http://localhost:${port}`);
+     })
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
---
+++ project/Models/IndividualTrainee.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Courses = require('./Courses');
+//Creating the IndividualTraineeSchema
+const IndividualTraineeSchema = new Schema
+(
+  {
+    firstName:
+    {
+      type: String,
+      required: true,
+    },
+    lastName: 
+    {
+      type: String,
+      required: true,
+    },
+    email: 
+    {
+      type: String,
+      required: true
+    },
+    username: 
+    {
+      type: String,
+      required: true,
+    },
+    password: 
+    {
+      type: String,
+      required: true
+    },
+    gender: 
+    {
+      type: Boolean,
+      required: true
+    },
+    country: 
+    {
+      type: String,
+      required: true,
+    },
+    courses: 
+    [
+        {
+        CourseID: 
+          {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref : 'Courses'
+          }
+        }
+    ],
+    wallet: 
+    {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }, { timestamps: true }
+);
+//Modeling the IndividualTraineeSchema in the MongoDb Cluster and exporting into into usable variable
+const IndividualTrainee = mongoose.model('IndividualTrainee', IndividualTraineeSchema);
+module.exports = IndividualTrainee;
