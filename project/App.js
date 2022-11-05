8132304db9c028d10db87d850a88eb4f102c0a77
Karim Wahdan <104279447+karim-walid-wahdan@users.noreply.github.com>
2022-11-06 00:40:50 +0100

    Update App.js

--- project/App.js
+++ project/App.js
@@ -14,7 +14,5 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/course', require('./Routes/coursesRoutes'));
-
 app.use("/instructor",require("./Routes/instructorRoutes"));
 app.use('/admin' , require('./Routes/adminRoutes'));
-
