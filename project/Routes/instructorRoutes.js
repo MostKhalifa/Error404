d492acb02641825c8b6238395f6b9bbdd9e1cb54
aynader <a.y.nader@outlook.com>
2022-11-06 02:55:12 +0200

    requirement 5,23

--- project/Routes/instructorRoutes.js
+++ project/Routes/instructorRoutes.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router  = express.Router();
 const 
-    {   
-        setInstructorCountry
+    {   setInstructorCountry,
         createNewCourse,
         viewAllInstructorCourses,
         filterInstructorCourses,
         searchInstructorCourses
     }= require("../Controller/instructorController");
-    
 router.route('/setCountry/:id').put(setInstructorCountry)
 router.route("/ViewAllCourses/:id").get(viewAllInstructorCourses);
 router.route("/FilterCourses/:id").get(filterInstructorCourses);
 router.route("/SearchCourses/:id").get(searchInstructorCourses);
 router.route("/AddCourse/:id").put(createNewCourse);
+
 module.exports = router;
