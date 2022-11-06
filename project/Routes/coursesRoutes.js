8be7c8b4ef8f3cf977cc2a9546a811f5bd335a9d
MostKhalifa <mostafa.mohamed360@gmail.com>
2022-11-06 01:54:45 +0100

    Update coursesRoutes.js

--- project/Routes/coursesRoutes.js
+++ project/Routes/coursesRoutes.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD
+
 const {
     getCourse,
     viewCoursePrice,
-  } = require('../Controller/coursesController')
-
-  router.route('/getCourses').get(getCourse)
-  router.route('/getCoursePrice/:id').get(viewCoursePrice)
-
-  module.exports = router;
-=======
-
-const {
     filterCourseSubject,
     filterCourseRating,
     filterCourseSubjectRating,
@@ -22,17 +13,15 @@ const {
     searchForCoursebyInstructor
   } = require('../Controller/coursesController')
 
-   router.route('/filterCourseSubject').get(filterCourseSubject)
-   router.route('/filterCourseRating').get(filterCourseRating)
-   router.route('/filterCourseSubjectRating').get(filterCourseSubjectRating)
-   router.route('/filterCoursePrice').get(filterCoursePrice)
-   router.route('/searchForCoursebyTitle').post(searchForCoursebyTitle)
-   router.route('/searchForCoursebySubject').post(searchForCoursebySubject)
-   router.route('/searchForCoursebyInstructor').post(searchForCoursebyInstructor)
+  router.route('/getCourses').get(getCourse)
+  router.route('/getCoursePrice/:id').get(viewCoursePrice)
+  router.route('/filterCourseSubject').get(filterCourseSubject)
+  router.route('/filterCourseRating').get(filterCourseRating)
+  router.route('/filterCourseSubjectRating').get(filterCourseSubjectRating)
+  router.route('/filterCoursePrice').get(filterCoursePrice)
+  router.route('/searchForCoursebyTitle').post(searchForCoursebyTitle)
+  router.route('/searchForCoursebySubject').post(searchForCoursebySubject)
+  router.route('/searchForCoursebyInstructor').post(searchForCoursebyInstructor)
 
    
-
-  
   module.exports = router
-  
->>>>>>> 978144c841dbc5aa814ca69f1b8fc10e09a0b567
