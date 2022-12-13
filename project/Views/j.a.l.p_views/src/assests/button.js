a481d0d31c2986223d5b91fc9e60538da01985bf
Karim Wahdan <karim.walid.wahdan@gmail.com>
2022-12-13 14:25:20 +0100

    sprint two req
    the UI and backend elements of sprint two
    -karim

--- project/Views/j.a.l.p_views/src/assests/button.js
+++
@@ -1,9 +0,0 @@
-import "../styling/buttons.css"
-function Button ({type,id,disable,content,icon,onClickfunction})
-{
-    return (
-        <button className={type} id={id} disabled={disable} onClick={onClickfunction}>{icon}{content}</button>
-        );
-        
-}
-export default  Button;
